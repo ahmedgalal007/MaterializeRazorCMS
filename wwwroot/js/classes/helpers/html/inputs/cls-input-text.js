79fb0a72b9ccb00d235fc73c3d8e073c4f08f4a3
Ahmed Galal <ahmedgalal007@hotmail.com>
2025-04-26 10:59:30 +0300

    Begin Inputs Helpers

---
+++ wwwroot/js/classes/helpers/html/inputs/cls-input-text.js
@@ -0,0 +1,13 @@
+/**
+ * Class page dataTable
+ */
+
+'use strict';
+
+export class clsInputText extends HTMLDivElement {
+
+  constructor(selector, page, isModal) { }
+}
+
+window.customElements.define('input-text', clsInputText, { extends: 'div' });
+export default clsInputText;
